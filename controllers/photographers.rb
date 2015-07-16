7841a3d577e6b2618cca43af6f3bb3478e86daa4
Kristin Beltz <beltz08@me.com>
2015-07-16 16:01:04 -0500

    Progress on Photographer CRUD stuff.

--- controllers/photographers.rb
+++ controllers/photographers.rb
@@ -1,3 +1,18 @@
 get "/photographers_main" do
   erb :"/photographers/photographers_main"
 end
+
+get "/create_photographers" do
+  erb :"/photographers/create_photographers"
+end
+
+get "/save_photographer" do
+  Photographer.create(name: params["name"])
+  erb :"/main/success"
+end
+
+get "/read_photographers" do
+  # Photographer.all
+  # iterate and stuff.
+
+end
