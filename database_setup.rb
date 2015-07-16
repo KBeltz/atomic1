006f962fd553c77eccf80bb84a6df4bd7f8d9a08
Kristin Beltz <beltz08@me.com>
2015-07-16 12:26:53 -0500

    bridge table for many-to-many relationship

--- database_setup.rb
+++ database_setup.rb
@@ -9,13 +9,18 @@ unless ActiveRecord::Base.connection.table_exists?(:photos)
   ActiveRecord::Base.connection.create_table :photos do |t|
     t.text :title
     t.integer :photographer_id
-    t.integer :album_id
   end
 end
 
 unless ActiveRecord::Base.connection.table_exists?(:albums)
   ActiveRecord::Base.connection.create_table :albums do |t|
     t.text :album_title
+  end
+end
+
+unless ActiveRecord::Base.connection.table_exists?(:albums_photos)
+  ActiveRecord::Base.connection.create_table :albums_photos do |t|
+    t.text :album_id
     t.integer :photo_id
   end
 end
