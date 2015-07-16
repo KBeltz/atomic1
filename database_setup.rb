2d641c461409a0a15d6dd915715d260cfd6ee73c
Kristin Beltz <beltz08@me.com>
2015-07-16 13:11:03 -0500

    Database setup changes. Foreign key nonsense

--- database_setup.rb
+++ database_setup.rb
@@ -8,12 +8,14 @@ unless ActiveRecord::Base.connection.table_exists?(:photos)
   ActiveRecord::Base.connection.create_table :photos do |t|
     t.text :title
     t.integer :photographer_id
+    t.integer :album_id
   end
 end
 
 unless ActiveRecord::Base.connection.table_exists?(:albums)
   ActiveRecord::Base.connection.create_table :albums do |t|
     t.text :album_title
+    t.integer :photo_id
   end
 end
 
