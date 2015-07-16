b7aa73cca2d4b0ffa49b292ae484f07f3920c380
Kristin Beltz <beltz08@me.com>
2015-07-16 14:16:34 -0500

    top photo method

--- models/photo.rb
+++ models/photo.rb
@@ -1,4 +1,14 @@
 class Photo < ActiveRecord::Base
   belongs_to :photographer
   has_and_belongs_to_many :albums
+
+  validates :title, presence: true
+
+  def self.top_photo?(photo)
+    if photo.albums.length >= 3
+      true
+    else
+      false
+    end
+  end
 end
